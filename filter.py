3319da977753bbe1a90975666280586268b61f68
Bevan Philip <git@bphilip.uk>
2020-11-19 18:00:12 +0000

    Migrate filter to LXML to improve speed.
    Former-commit-id: 468e20e7b5c47c8106e80f246dc9ad1cfd13ea59

--- filter.py
+++ filter.py
@@ -1,60 +1,50 @@
-from xml.dom import minidom
+from lxml import etree
 from tqdm import tqdm
 import argparse
-
-def pretty_print(data): 
-    """
-    Removes the whitespace from XML output.
-    Credit to: https://stackoverflow.com/questions/14479656/empty-lines-while-using-minidom-toprettyxml
-    """
-    return '\n'.join([line for line in minidom.parseString(
-    data).toprettyxml(indent=' '*2).split('\n') if line.strip()])
-
-def remove_element(parent_element, name_of_child):
-    namespaces = parent_element.getElementsByTagName(name_of_child)[0]
-    parent = namespaces.parentNode
-    parent.removeChild(namespaces)
 
 if __name__ == "__main__":
     # Parses the arguments for input file and destination.
     parser = argparse.ArgumentParser()
     parser.add_argument('input', type=str,
-                    help='name of input filename')
+                        help='name of input filename')
     parser.add_argument('dest', type=str,
-                    help='name of destination filename')
+                        help='name of destination filename')
     args = parser.parse_args()
-    
 
-    print("Reading MediaWiki XML file. This may take a little bit of time.")
-    xmldoc = minidom.parse(args.input)
+    print("Reading MediaWiki XML file.")
+    parsed = etree.parse(args.input)
+    tree = parsed.getroot()
 
     print("Stripping namespaces")
     # Here, we strip all the namespace definitions. We have no need for this.
-    remove_element(xmldoc, "namespaces")
-    itemlist = xmldoc.getElementsByTagName('page')
+    namespaces = tree.xpath('siteinfo/namespaces')
+    for elem in tqdm(namespaces):
+        elem.getparent().remove(elem)
 
-    print("Stripping information from each page.")
+    print("Cleaning up tags.")
     # tqdm adds a progress bar. Useful for such a long operation.
-    for item in tqdm(itemlist):
-        redirect = item.getElementsByTagName('redirect')
-        remove_elements = ["contributor", "comment", "parentid", "model", "format", "timestamp", "minor", "ns"]
-        text = item.getElementsByTagName('text')[0].firstChild.nodeValue
+    # Clean up the tags
+    for elem in tqdm(tree.iterdescendants()):
+        elem.tag = etree.QName(elem).localname
 
+    print("Removing pages with redirects")
+    redirect = tree.xpath('page/redirect')
+    for elem in tqdm(redirect):
+        page = elem.getparent()
+        page.getparent().remove(page)
 
-        # Remove any redirect or disambiguation pages. Pollutes our data.
-        if len(redirect) >= 1 or "{{disambig}}" in text.lower():
-            parent = item.parentNode
-            parent.removeChild(item)
-        else:
-            for remove_target in remove_elements:
-                try:
-                    remove_element(item, remove_target)
-                # Certain tags may only be on some pages. This doesn't matter, we just ignore the error.
-                except Exception:
-                    pass
+    remove_elements = ["contributor", "comment", "parentid",
+                       "model", "format", "timestamp", "minor", "ns"]
+    print("Cleaning up unneeded information")
+    for remove in tqdm(remove_elements):
+        for subelem in tree.iterfind('.//%s' % remove):
+            subelem.getparent().remove(subelem)
 
+    for elem in tree.iterfind('.//%s' % "text"):
+        if "{{disambig}}" in elem.text.lower():
+            page = elem.getparent().getparent()
+            page.getparent().remove(page)
 
-    print("Writing cleaned XML to disk.")
-    with open(args.dest, "w", encoding="utf-8") as writeFile:
-        writeFile.write(pretty_print(xmldoc.toxml()))
-    print("Completed.")
+    etree.cleanup_namespaces(tree)
+    with open("test.xml", "wb") as openFile:
+        openFile.write(etree.tostring(tree, pretty_print=True))
